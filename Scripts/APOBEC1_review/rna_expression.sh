859c82ed5dd0be464823367bd0c18aa39579ed94
Aswin S Soman <ceglab3@gmail.com>
2026-03-16 13:41:09 +0530

    code update

--- Scripts/APOBEC1_review/rna_expression.sh
+++ Scripts/APOBEC1_review/rna_expression.sh
@@ -20,8 +20,7 @@
 ##########################################################################################################################################################################################################################################################################################################
 #Download RNA seq data
 
-
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+##########################################################################################################################################################################################################################################################################################################
 #Casuarius casuarius
 
 #Download SRA: unknown sex , unknown tissue type: 1.5Gb
@@ -33,7 +32,7 @@ datasets download genome accession GCA_013396415.1 --include genome,gtf,seq-repo
 unzip GCA_013396415.1.zip -d GCA_013396415.1
 datasets rehydrate --directory GCA_013396415.1
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+##########################################################################################################################################################################################################################################################################################################
 #Leptosomus discolor
 
 #Download SRA : unknown sex , blood: 1.1Gb
@@ -41,18 +40,21 @@ cd /media/ashutosh/disk3/RNA_seq
 time /media/ashutosh/disk3/RNA_seq/sratoolkit.3.3.0-ubuntu64/bin/prefetch  --max-size 100000000 SRR10853056
 
 
-#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+##########################################################################################################################################################################################################################################################################################################
 #Rhea_americana
 
-#-------------------------------------------------------
-#Fetch SRA metadata
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#Download using aspera 
 
+#Fetch SRA metadata first to get filter samples & extract their ftp links to download using aspera
 cd ~/soft_links/Rhea_americana/genome
 time sra-meta -s Rhea_americana > Rhea_americana_sra
 
 #There are mainly 2 projects: one project has only ine sample & 2nd one has 24 samples
 #Download one manually: 4m28.902s
 time /home/neo/.aspera/connect/bin/ascp -k2 -QT -l 300m -P33001 -i /home/neo/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:/vol1/fastq/SRR108/033/SRR10852933/SRR10852933_1.fastq.gz .
+time /home/neo/.aspera/connect/bin/ascp -k2 -QT -l 300m -P33001 -i /home/neo/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:/vol1/fastq/SRR108/033/SRR10852933/SRR10852933_2.fastq.gz .
+
 #Get project number & search in ENA browser & select the column names & download table as tsv file, 
 #Check columns, especially "fastq ftp" & "Library Strategy" & "library layout" & "Library Source" & "Experiment Title" 
 grep -if <(grep RNA Rhea_americana_sra | awk '{print$1}') ENA_PRJNA1338461_table.tsv | awk -F "\t" '{print$NF}' | awk -F ";" '{print$2"\n"$3}' | awk '{print"time /home/neo/.aspera/connect/bin/ascp -k2 -QT -l 300m -P33001 -i /home/neo/.aspera/connect/etc/asperaweb_id_dsa.openssh era-fasp@"$0,"."}' > ascp_links.sh
@@ -72,16 +74,25 @@ done
 mkdir /media/ashutosh/disk3/RNA_seq/Rhea_americana
 #In ceglab25
 time scp SRR*.fa workstation@172.30.1.172:/media/ashutosh/disk3/RNA_seq/Rhea_americana/
+time scp SRR*.gz workstation@172.30.1.172:/media/ashutosh/disk3/RNA_seq/Rhea_americana
 #In Z840
 cd /media/ashutosh/disk3/RNA_seq/Rhea_americana
 #14m13.799s
 time cat SRR*.fa > combined_SRA_Rhea_americana.fa
 
-#-------------------------------------------------------
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #RNA mapping:
 
+samtools faidx GCA_003343005.1_rheAme1_genomic.fna
+
 #Index the genome from mapping
-time STAR --runThreadN 8 --runMode genomeGenerate --genomeDir . --genomeFastaFiles GCF_000247815.1_FicAlb1.5_genomic.fna --sjdbGTFfile GCF_000247815.1_FicAlb1.5_genomic.gff --limitGenomeGenerateRAM 31000000000
+time /home/workstation/aswin/programs/STAR/source/STAR --runThreadN 64 --runMode genomeGenerate --genomeDir . --genomeFastaFiles GCA_003343005.1_rheAme1_genomic.fna --limitGenomeGenerateRAM 31000000000
+read_1="SRR10852933_1.fastq.gz"
+read_2="SRR10852933_2.fastq.gz"
+id="SRR10852933"
+ulimit -n 65535
+time /home/workstation/aswin/programs/STAR/source/STAR --runThreadN 16 --genomeDir . --readFilesCommand zcat --readFilesIn $read_1 $read_2 --outFileNamePrefix "${id}_aligned_" --outSAMtype BAM SortedByCoordinate
+
 #STAR Mapping
 time for i in *_1.fastq
 do
@@ -90,4 +101,6 @@ k=`echo $i|sed 's/_1.fastq//g'`
 STAR --runThreadN 8 --outSAMtype BAM SortedByCoordinate --genomeDir . sjdbGTFfile GCF_000247815.1_FicAlb1.5_genomic.gff --readFilesIn $i $j  --outFileNamePrefix "$k"_brain
 samtools index *.bam
 done
+
+/home/workstation/aswin/programs/STAR/source/STAR
 
