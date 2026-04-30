75889b44c6f2337eb2dbfa85da2551ba715cf5d3
Aswin S Soman <ceglab3@gmail.com>
2026-04-30 17:52:56 +0530

    update

--- Scripts/APOBEC1_review/dna_editing_association_draft.sh
+++ Scripts/APOBEC1_review/dna_editing_association_draft.sh
@@ -25,23 +25,23 @@ colnum.sh /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotr
 #repeatmasker
 head /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/Geospiza_fortis/ucsc/GCF_000277835.1.repeatMasker.out
 
+awk '{print $1,NF-1}'/media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/Geospiza_fortis/knisbacher/Data/Geofor/LTR/db/Len_ERVL.txt
+
+grep TguLTRL4d ../../../../../ucsc/GCF_000277835.1.repeatMasker.out | awk '$NF!="*"' | awk '{print$10,$7-$6+1}' | less
+
+#Total ERVs used by knisbacher script
+cd /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/Geospiza_fortis/knisbacher/Data/Geofor/LTR/db
+awk '$11~"ERV"' ../../../../../ucsc/GCF_000277835.1.repeatMasker.out | awk '$NF!="*"' | wc -l
+awk '{print $1,NF-1}' Len_ERVL.txt | awk '{a+=$2} END{print a}'
 
 
-cd /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/Geospiza_fortis/knisbacher/Data/Geofor/LTR/results
 
-while read i
-do
-chr=$(echo $i | cut -f1,2 -d "_" | tr -d ">")
-start=$(echo $i | sed 's/\.\..*//g' | awk -F "_" '{print$NF}')
-end=$(echo $i | sed 's/.*\.\.//g' | awk -F "_" '{print$1}')
-subfam=$(echo $i | sed 's/.*\.\.//g' | awk -F "_" '{print$4}')
-awk -v chr="$chr" -v start="$start" -v end="$end" -v subfam="$subfam" '$10~subfam && $5==chr && $6==start && $7==end' ../../../../../ucsc/GCF_000277835.1.repeatMasker.out
-unset chr start end subfam
-done < <(grep ">" ../../../../../transposons/ERVs.fa) > erv_repeatmasker.out
+
 
 ################################################################################################################################################################################################################################################################################################################
 #ERV divergence and edit site count table summary
 
+#Geospiza_fortis
 cd /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/Geospiza_fortis/knisbacher/Data/Geofor/LTR/results
 
 ga="GA"
@@ -78,4 +78,108 @@ unset chr rstart cstart end subfam p1 p2
 done < Tracks/tracks_Geofor_LTR_ERVL_1e-0_5/GA/pairwise_filter/bestPairsClusters_Geofor_LTR_ERVL_1e-0_5.tab \
  | sed '1i mismatch species Repeat_type family subfamily chromosome start end length GA_count CT_count 2nd_highest_count subtrated_GA_count SW_score Per_Div Per_Del Per_Ins' | sed 's/[ \t]\+/\t/g' > edited_repeatmasker_edit_count.out
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Filter repeatmasker for selected ERVs
+
+awk '$11~"ERV"' ../../../../../ucsc/GCF_000277835.1.repeatMasker.out | awk '$NF!="*"' > erv_repeatmasker.out
+awk '{print$2}' erv_repeatmasker.out | statplot.R --biplot divergence_distribution_erv_repeatmasker.out.png
+
+awk '$11~"ERV"' ../../../../../ucsc/GCF_000277835.1.repeatMasker.out | awk '$NF!="*"' | awk '{print$10,$5,$6-1,$7,$2}' > f1.out
+awk '{print$5,$6,$7,$8,$NF}' edited_GA_count > f2.out
+
+awk 'NR==FNR {key=$1$2$3$4; val[key]=$5; next} {key=$1$2$3$4; if (key in val) print $0, val[key]; else print $0, 0}' f2.out f1.out \
+	| sed '1i Subfamily chr start end percent_divergence GA_edit_count' | sed 's/[ \t]\+/\t/g' > erv_div_ga_edit_count.out
+
+Rscript plot_div_vs_edit.R erv_div_ga_edit_count.out erv_div_ga_edit_count.pdf
+
+################################################################################################################################################################################################################################################################################################################
+
+species=Gallus_gallus
+o=$(echo $species | awk -F "_" '{print substr($1,1,3)""substr($2,1,3)}')
+
+cd /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/"$species"/knisbacher
+mkdir repeat_insertion_time_DNA_editing_association
+
+#Set mismatch type
+ga="GA"
+ganum=$(echo "GA CT GC GT CA TA AG TC CG TG AC AT" | awk -v m="$ga" '{for(i=1;i<=NF;i++) if($i == m) print i}')
+ct="CT"
+ctnum=$(echo "GA CT GC GT CA TA AG TC CG TG AC AT" | awk -v m="$ct" '{for(i=1;i<=NF;i++) if($i == m) print i}')
+
+#Get repeatmasker file
+if [[ -d ../ucsc ]]
+then
+rmsk=$(ls ../ucsc/*.repeatMasker.out)
+else
+rmsk=$(ls ../repeat_masker_using_"$species"_library/*.fna.out)
+fi
+
+#Total ERV count from blastdb of knisbacher pipeline & manual selection of repeatmasker table: These 2 values should match
+awk '$11~"ERV"' $rmsk | awk '$NF!="*"' | wc -l
+for r in $(ls Data/"$o"/LTR/db/Len_*.txt | grep -v "Len_.txt"); do r1=$(echo $r | awk -F "/" '{print$NF}')
+awk '{print $1,NF-1}' $r | awk '{a+=$2} END{print a}' | sed "s/^/$r1 /g"
+done |  awk '{a+=$2} END{print a}'
+
+#Filter repeatmasker for selected ERVs
+awk '$11~"ERV"' $rmsk | awk '$NF!="*"' > repeat_insertion_time_DNA_editing_association/erv_repeatmasker.out
+awk '{print$2}' erv_repeatmasker.out | statplot.R --biplot divergence_distribution_erv_repeatmasker.out.png
+
+for bestpairs in $(find Data/"$o"/LTR/results -maxdepth 5 -mindepth 5 -path "*/Tracks/*/${ga}/pairwise_filter*" -name "bestPairsClusters_*.tab")
+do
+while read -r bp
+do
+s=$(echo $bp | awk '{print$1,$2,$3,$4,$5,$6}' | awk '{split($NF,a,/[:\-+]/); s=substr($NF,length($NF),1); $(NF)=a[1]; print $0, a[2], a[3], s}')
+c=$(echo $bp | awk '{print$NF}')
+i1=$(echo $c | cut -f $ganum -d "|")
+i2=$(echo $c | tr "|" "\n" | sed "$ganum d" | sort -nr | head -1)
+i3=$(calc $i1 - $i2)
+i4=$(echo $c | cut -f $ctnum -d "|")
+subfam=$(echo $s | awk '{print$5}')
+chr=$(echo $s | awk '{print$6}')
+start=$(echo $s | awk '{print$7+1}')
+end=$(echo $s | awk '{print$8}')
+strand=$(echo $s | awk '{print$9}' | awk '{if($1=="+") print$1; else print"C"}')
+p1=$(awk -v chr="$chr" -v start="$start" -v end="$end" -v subfam="$subfam" -v strand="$strand" '$10~subfam && $5==chr && $6==start && $7==end && $9==strand' $rmsk | awk '{print$1,$2,$3,$4}')
+echo $s $i1 $i4 $i2 $i3 $p1
+unset s c i1 i2 i3 i4 subfam chr start end p1 strand
+done < $bestpairs
+unset bp
+done | awk '{print$1,$2,3,4,5,6,7}' > repeat_insertion_time_DNA_editing_association/erv_per_div_GA_edit_count.out
+
+awk '{print$5,$6,$7+1,$8,$9,$13}' repeat_insertion_time_DNA_editing_association/erv_per_div_GA_edit_count.out | awk '{if($5=="-") $5="C"; print}' > repeat_insertion_time_DNA_editing_association/c1
+
+
+awk '{$7=$7+1; print}' repeat_insertion_time_DNA_editing_association/erv_per_div_GA_edit_count.out | awk '{if($9=="-") $9="C"; print}' > repeat_insertion_time_DNA_editing_association/c1
+
+
+cd repeat_insertion_time_DNA_editing_association/
+
+
+
+################################################################################################################################################################################################################################################################################################################
+#DRAFT FOR DRAFT
+
+cd /media/aswin/gene_loss/APOBEC1/hypermutation_analyses/identify_retrotransposons/Geospiza_fortis/knisbacher/Data/Geofor/LTR/results
+
+#Filter repeatmasker for selected ERVs
+while read i
+do
+chr=$(echo $i | cut -f1,2 -d "_" | tr -d ">")
+start=$(echo $i | sed 's/\.\..*//g' | awk -F "_" '{print$NF}')
+end=$(echo $i | sed 's/.*\.\.//g' | awk -F "_" '{print$1}')
+subfam=$(echo $i | sed 's/.*\.\.//g' | awk -F "_" '{print$4}')
+awk -v chr="$chr" -v start="$start" -v end="$end" -v subfam="$subfam" '$10~subfam && $5==chr && $6==start && $7==end' ../../../../../ucsc/GCF_000277835.1.repeatMasker.out
+unset chr start end subfam
+done < <(grep ">" ../../../../../transposons/ERVs.fa) > erv_repeatmasker.out
+  
+##Filter repeatmasker for selected ERVs (faster)
+awk 'FNR==NR && /^>/{
+  sub(/^>/,""); split($0,a,"_"); chr=a[1]"_"a[2]
+  split($0,b,/\.\./)
+  split(b[1],c,"_"); start=c[length(c)]
+  split(b[2],d,"_"); key[chr"|"start"|"d[1]"|"d[4]]
+  next}
+{k=$5"|"$6"|"$7"|"$10
+  if (k in key) print}' ../../../../../transposons/ERVs.fa ../../../../../ucsc/GCF_000277835.1.repeatMasker.out > erv_repeatmasker.out
 
